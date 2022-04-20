9f5c38c24118695297c3557e9a9d0cc08c2d0e2b
ViniciusBirreik <viniciusbirreik@gmail.com>
2022-04-20 20:59:58 -0300

    First Commit

---
+++ src/app.ts
@@ -0,0 +1,45 @@
+import express from 'express'
+import cors from 'cors'
+
+import Database from './database/connections/database';
+import RecadosRoutes from './routers/recados';
+
+export default class Application {
+    readonly #express: express.Application;
+
+    constructor() {
+        this.#express = express();
+    }
+
+    async init() {        
+        this.config();
+        // this.middlewares();
+        this.routers();
+        await this.database();
+    }
+
+    start(port: number) {
+        this.#express.listen(port, () => {
+            console.log(`A aplicação está rodando na porta ${port}...`); 
+        });
+    }
+
+    private config() {
+        this.#express.use(express.json());
+        this.#express.use(express.urlencoded({ extended: false }));
+        this.#express.use(cors());
+    }
+
+    // private middlewares() {
+    //     // TODO
+    // }
+
+    private routers() {
+        const recadosRoutes = new RecadosRoutes().init()
+        this.#express.use(recadosRoutes)
+    }
+
+    private async database() {
+        await Database.getInstance();
+    }
+};
---
+++ src/database/entities/Recados.ts
@@ -0,0 +1,20 @@
+import {Entity, BaseEntity, PrimaryColumn, Column} from "typeorm";
+
+@Entity()
+export class Recados extends BaseEntity {
+    @PrimaryColumn()
+    id?: number
+
+    @Column()
+    descricao: string
+
+    @Column()
+    detalhes: string
+
+    constructor(descricao: string, detalhes: string){
+        super()
+        this.descricao = descricao
+        this.detalhes = detalhes
+    }
+
+}
