2de0f11912ae7b0de4235abb9f875d093ba67f8c
ViniciusBirreik <viniciusbirreik@gmail.com>
2022-04-21 20:41:00 -0300

    Adicionei repositories e service

--- src/controllers/recados.ts
+++ src/controllers/recados.ts
@@ -1,48 +1,29 @@
 import { Request, Response } from "express";
 
-import { Recados } from "../database/entities/Recados";
+import { RecadosService } from "../service/recados";
 
 export default class RecadosController {
     async index(request: Request, response: Response){
-        return response.json(await Recados.find())
+        
     }
     
     async show(request: Request, response: Response){
         const { id } = request.params
 
-        return response.json(await Recados.findOne(id))
     }
 
     async store(request: Request, response: Response){
         const { descricao, detalhes} = request.body
         
-        return response.json( await new Recados(descricao, detalhes).save())
     }
 
     async update(request: Request, response: Response){
         const { id } = request.params
         const { descricao, detalhes } = request.body
-        const recados = await Recados.findOne(id)
-
-        if (recados){
-            recados.descricao = descricao
-            recados.detalhes = detalhes
-            recados.save()
-        }else{
-            return response.status(404).json({
-                mensagem: 'Recado não encontrado'
-            })
-        }
-
-        return response.json(recados)
     }
 
     async delete(request: Request, response: Response){
         const { id } = request.params
-
-        await Recados.delete(id)
-
-        return response.sendStatus(204)
-
+        
     }
 }
