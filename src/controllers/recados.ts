46d39c7f9bc6f3e1c786ecd24f6869ae6a4b1afb
ViniciusBirreik <viniciusbirreik@gmail.com>
2022-04-22 19:29:27 -0300

    Atualizando o CRUD

--- src/controllers/recados.ts
+++ src/controllers/recados.ts
@@ -4,26 +4,57 @@ import { RecadosService } from "../service/recados";
 
 export default class RecadosController {
     async index(request: Request, response: Response){
-        
+        const service = new RecadosService()
+        const recados = await service.find()
+
+        return response.json(recados.map(recado => {
+            return {
+                descricao: recado.descricao.toUpperCase(),
+                detalhes: recado.detalhes.toUpperCase()
+            }
+        }))
     }
     
     async show(request: Request, response: Response){
         const { id } = request.params
+        const service = new RecadosService()
+        const recados = await service.findOne(parseInt(id))
 
+        return response.json({
+            descricao: recados?.descricao.toUpperCase(),
+            detalhes: recados?.detalhes.toUpperCase()
+        })
     }
 
     async store(request: Request, response: Response){
         const { descricao, detalhes} = request.body
-        
+        const service = new RecadosService()
+        const recados = service.create({
+            descricao: descricao,
+            detalhes: detalhes
+        })
+
+        return response.json(recados)
     }
 
     async update(request: Request, response: Response){
         const { id } = request.params
         const { descricao, detalhes } = request.body
+        const service = new RecadosService()
+        const recados = service.update({
+            id: parseInt(id),
+            descricao: descricao,
+            detalhes: detalhes
+        })
+
+        return response.json(recados)
     }
 
     async delete(request: Request, response: Response){
         const { id } = request.params
-        
+        const service = new RecadosService()
+        service.delete(parseInt(id))
+
+        return response.status(204)
     }
 }
